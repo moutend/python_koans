fccb139c966659d32d2a44006d60b1ad4b60dd32
Yoshiyuki Koyanagi <moutend@gmail.com>
2016-10-04 12:43:49 +0900

    Add about triangle project 2

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -18,14 +18,21 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    s = set([a, b, c])
-    if len(s) == 1:
-        return 'equilateral'
-    elif len(s) == 2:
+    xs = sorted([a, b, c])
+
+    for x in xs:
+        if not (x > 0): raise TriangleError
+
+    is_triangle = (xs[0] + xs[1]) > xs[2]
+    if not is_triangle:
+        raise TriangleError
+
+    if len(set(xs)) == 3:
+        return 'scalene'
+    elif len(set(xs)) == 2:
         return 'isosceles'
     else:
-        return 'scalene'
+        return 'equilateral'
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(StandardError):
